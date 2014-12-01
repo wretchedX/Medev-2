ed1d0a5b956a17c05f7cc47502d0203fb49cd8ca
wretchedX <thest4lker@gmail.com>
2014-12-01 11:08:46 +0100

    Endl c'est bcp mieux

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 1 décembre 2014, 11:04
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "qq";
+    return 0;
+}
+
