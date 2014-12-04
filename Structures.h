cb3b69d215b014331b2b4723c603cefb7b3a04e8
MaxBellot <maximilien.bellot@eleves.ec-nantes.fr>
2014-12-04 08:02:06 +0100

    changement de nom de "Pierre.h" en "Structures.h"

---
+++ Structures.h
@@ -0,0 +1,20 @@
+/* 
+ * File:   Pierre.h
+ * Author: eleve
+ *
+ * Created on 1 décembre 2014, 15:48
+ */
+
+#ifndef PIERRE_H
+#define	PIERRE_H
+
+enum Pierre {BLANC, NOIR, VIDE}
+
+struct Position {
+    int x;
+    int y;
+}
+
+
+#endif	/* PIERRE_H */
+
